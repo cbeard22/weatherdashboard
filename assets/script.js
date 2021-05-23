3b7cfd90f5a1d1fdf0e817844e973575adb3ed86
Chris Beard <cbeard@hnoj.org>
2021-05-22 21:50:23 -0500

    added current location information and appended search

--- assets/script.js
+++ assets/script.js
@@ -11,15 +11,19 @@ let inputQuery = "name=" + query;
 let apiKey = "&appid=8bd9dc1af4a9dc570c14c41c2c1718ce";
 let requestURL;
 
-function getLatLon(event) {
+function getLatitudeLongitude(event) {
     event.preventDefault();
     let query = inputValue.val();
     requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial" + apiKey;
+    localStorage.setItem("Name", query);
+    localStorage.getItem("Name")
+    let btn = document.createElement("BUTTON");
+    btn.innerHTML = (query);
+    document.body.append(btn);
 
-    $.ajax({url:requestURL, method: "GET"})
-        /*.then(function (response) {
-            return response.json();
-        })*/
+
+    $.ajax({ url: requestURL, method: "GET" })
+
         .then(function (data) {
             let lineData = data.coord;
             weatherData(lineData);
@@ -28,37 +32,37 @@ function getLatLon(event) {
 }
 
 function weatherData(coordinates) {
-    let lon=coordinates.lon;
-    let lat=coordinates.lat;
-    requestURL="https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" + apiKey;
+    let lon = coordinates.lon;
+    let lat = coordinates.lat;
+    requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" + apiKey;
 
-    $.ajax({url:requestURL, method: "GET"})
-       /* .then(function (response) {
-            return response.json();
-        })*/
+    $.ajax({ url: requestURL, method: "GET" })
+
         .then(function (data) {
-            let icon=$("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
-            console.log(icon);
+            let icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
 
-            let temp="Temp: " + data.current.temp + "\u00B0" + "F";
-            let wind="Wind: " + data.current.wind_speed + "MPH";
-            let humidity="Humidity: " + data.current.humidity + "%";
-            let uvIndex="UV Index: " + data.current.uvi;
 
-           /* iconEl.empty();
+            let temp = "Temp: " + data.current.temp + "\u00B0" + "F";
+            let wind = "Wind: " + data.current.wind_speed + "MPH";
+            let humidity = "Humidity: " + data.current.humidity + "%";
+            let uvIndex = "UV Index: " + data.current.uvi;
+
+            iconEl.empty();
             tempEl.empty();
             windEl.empty();
             humidityEl.empty();
-            uvIndexEl.empty();*/
+            uvIndexEl.empty();
 
             $("#icon").append(icon);
-            /*tempEl.append(temp);
+            tempEl.append(temp);
             windEl.append(wind);
             humidityEl.append(humidity);
-            uvIndexEl.append(uvIndex);*/
+            uvIndexEl.append(uvIndex);
         })
     return;
 }
 
-searchBtnEl.on("click", function(){
-    getLatLon()});
+
+searchBtnEl.on("click", function (event) {
+    getLatitudeLongitude(event)
+});
